3a094f84fe664f3ef622197f6542a257db2bffd8
DAIFALLAH Hakim <hakim.daifallah@groupeadequat.fr>
2020-10-30 16:17:37 +0100

    Add ngrx to load types from api
    Add rework with ngrx
    
    Add progress
    
    Add ngrx to load types from api

---
+++ src/app/home/store/types-display.reducer.ts
@@ -0,0 +1,23 @@
+import {Action, createReducer, on} from '@ngrx/store';
+import * as TypesDisplayActions from 'src/app/home/store/types-display.actions';
+
+export interface State {
+  types: [{name: string; url: string}];
+}
+
+export const initialState: State = {
+  types: undefined
+};
+
+const TypesDisplayReducer = createReducer(
+  initialState,
+  on(TypesDisplayActions.loadTypesSuccess, ((state, action) => ({
+      ...state, types: action.types.filter((type) => type.name !== 'unknown')
+    }))
+  ));
+
+export function reducer(state: State | undefined, action: Action) {
+  return TypesDisplayReducer(state, action);
+}
+
+export const typesDisplayFeatureKey = 'types';

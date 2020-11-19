7994cdd63e34a74159230c042fe6c2bfd1266f88
HakimDai <hakim69400@live.com>
2020-11-19 22:52:46 +0100

    Work in progress

--- src/app/research-type/services/research-type.service.ts
+++ src/app/research-type/services/research-type.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Type, types } from 'src/app/shared/typeEffectiveness';
 
 @Injectable({
   providedIn: 'root',
@@ -6,5 +7,23 @@ import { Injectable } from '@angular/core';
 export class ResearchTypeService {
   constructor() {}
 
-  searchType(selectedTypes) {}
+  searchType(selectedTypes: Set<Type>) {
+    let result = [];
+    switch (selectedTypes.size) {
+      case 0:
+        return alert('error');
+      case 1:
+        for (let type of types) {
+          console.log(type.type === selectedTypes[0].type);
+          if (type.type === selectedTypes[0].type) {
+            let index = type.damageTaken.indexOf(2);
+            while (index != -1) {
+              result.push({ type: types[index].type, damage: 2 });
+              index = type.damageTaken.indexOf(2, index + 1);
+            }
+          }
+        }
+        return alert(result);
+    }
+  }
 }

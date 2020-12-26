0a1e83654526567142fc175a639744dad9cd712f
Hakim Daifallah <54754083+HakimDai@users.noreply.github.com>
2020-12-26 15:03:18 +0100

    Fix apiUrl for prod and dev environnements (#25)

--- src/app/selectType/services/request-pokemon.service.ts
+++ src/app/selectType/services/request-pokemon.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { TypeDetails } from 'src/app/shared/models/typeDetails.model';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RequestPokemonService {
-  apiUrl = 'http://localhost:4200/api';
+  apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
